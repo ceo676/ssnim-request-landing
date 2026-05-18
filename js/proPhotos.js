ef5c3b1dbcd7c9e258c485c86e491f3a4d0ecfdb
rlaeodn <ceo@ssnim.com>
2026-05-18 19:29:02 +0900

    Add prosImg array with image URLs

---
+++ js/proPhotos.js
@@ -0,0 +1,18 @@
+// js/proPhotos.js
+export const prosImg = [
+    "https://rlaeodn1015-oss.github.io/ssnim-request-landing/images/KakaoTalk_20260327_165459882.png",
+    "https://rlaeodn1015-oss.github.io/ssnim-request-landing/images/JYH.jpg",
+    "https://rlaeodn1015-oss.github.io/ssnim-request-landing/images/KakaoTalk_20260309_200933587.jpg",
+    "https://rlaeodn1015-oss.github.io/ssnim-request-landing/images/SJH.png",
+    "https://rlaeodn1015-oss.github.io/ssnim-request-landing/images/HJY.png",
+    "https://rlaeodn1015-oss.github.io/ssnim-request-landing/images/YHC.png",
+    "https://rlaeodn1015-oss.github.io/ssnim-request-landing/images/JDY.png",
+    "https://rlaeodn1015-oss.github.io/ssnim-request-landing/images/KNY.jpg",
+    "https://rlaeodn1015-oss.github.io/ssnim-request-landing/images/lwy.webp",
+    "https://rlaeodn1015-oss.github.io/ssnim-request-landing/images/YMH.png",
+    "https://rlaeodn1015-oss.github.io/ssnim-request-landing/images/kdy.webp",
+    "https://rlaeodn1015-oss.github.io/ssnim-request-landing/images/KTY.png",
+    "https://rlaeodn1015-oss.github.io/ssnim-request-landing/images/LSJ.png",
+    "https://rlaeodn1015-oss.github.io/ssnim-request-landing/images/KDW.jpg", 
+    "https://rlaeodn1015-oss.github.io/ssnim-request-landing/images/LGH.webp"
+];
